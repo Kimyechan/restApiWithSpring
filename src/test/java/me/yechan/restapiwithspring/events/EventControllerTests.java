2a3af5f5b1c587436b2e60bec801b175f7b5d88b
Kimyechan <vlvkcjswo7@naver.com>
2019-12-29 01:05:04 +0900

    validate wrong data

--- src/test/java/me/yechan/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/yechan/restapiwithspring/events/EventControllerTests.java
@@ -32,7 +32,18 @@ public class EventControllerTests {
 
     @Test
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
-        EventDto eventDto = EventDto.builder().build();
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEventDateTime(LocalDateTime.of(2018,11,26,14,28))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,25,14,28))
+                .beginEventDateTime(LocalDateTime.of(2018,11,24,14,28))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,28))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2")
+                .build();
 
         this.mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON)
