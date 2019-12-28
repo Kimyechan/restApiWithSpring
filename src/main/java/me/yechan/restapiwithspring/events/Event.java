500d22f054b7829d9a912100b9d997439256e7af
Kimyechan <vlvkcjswo7@naver.com>
2019-12-28 19:11:15 +0900

    POST /api/events returns 201

--- src/main/java/me/yechan/restapiwithspring/events/Event.java
+++ src/main/java/me/yechan/restapiwithspring/events/Event.java
@@ -2,13 +2,15 @@ package me.yechan.restapiwithspring.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
-
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -22,6 +24,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 
