f20e871121b50680d8eb6199379619cf29e8dfad
Kimyechan <vlvkcjswo7@naver.com>
2019-12-28 18:20:12 +0900

    Event domain

---
+++ src/main/java/me/yechan/restapiwithspring/events/EventStatus.java
@@ -0,0 +1,5 @@
+package me.yechan.restapiwithspring.events;
+
+public enum  EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/me/yechan/restapiwithspring/events/EventTest.java
@@ -0,0 +1,31 @@
+package me.yechan.restapiwithspring.events;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflean Spring Rest API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        Event event = new Event();
+        String name = "Event";
+        String description = "Spring";
+
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
