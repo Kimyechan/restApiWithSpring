0c83786ac242bb10a365873d7d4b4c1fe6d6174c
Kimyechan <vlvkcjswo7@naver.com>
2019-12-29 01:10:57 +0900

    Add test description annotation

--- src/test/java/me/yechan/restapiwithspring/events/EventControllerTests.java
+++ src/test/java/me/yechan/restapiwithspring/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.yechan.restapiwithspring.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.yechan.restapiwithspring.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("입력값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
@@ -51,6 +53,7 @@ public class EventControllerTests {
                 .andExpect(status().isBadRequest());
     }
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -79,6 +82,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
